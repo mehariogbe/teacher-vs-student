1769684705ec35b938f900f5ad93c83fa8e36030
Mehari Abraha <mehariogbe@yahoo.com>
2021-11-10 14:47:56 -0600

    create student and teacher signup

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('register/',views.Register.as_view(), name='register'),
+    path('teacher_signup/',views.teacher_signup.as_view(), name='teacher_signup'),
+    path('student_signup/',views.student_signup.as_view(), name='student_signup'),
 
 
 
--- app/views.py
+++ app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+from django.views.generic import CreateView
+from .models import User
+from .form import StudentSignUpForm, TeacherSignUpForm
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 class Home(TemplateView):
@@ -10,5 +14,16 @@ class Home(TemplateView):
 # registration or signup
 
 class Register(TemplateView):
-    template_name = "register.html"
-     
+    template_name = "registration/register.html"
+
+class teacher_signup(CreateView):
+    model = User
+    form_class = TeacherSignUpForm
+    template_name= 'registration/teacher_signup.html'
+
+
+
+class student_signup(CreateView):
+    model = User
+    form_class = StudentSignUpForm
+    template_name= 'registration/student_signup.html'     
