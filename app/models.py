79aa6174d1880206c14263c94a1c5b2f43b22169
Mehari Abraha <mehariogbe@yahoo.com>
2021-11-10 23:00:56 -0600

    create NavBar

--- app/models.py
+++ app/models.py
@@ -8,8 +8,8 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
-    # def __str__(self):
-    #     return self.username
+    def __str__(self):
+        return self.username
 
 class Teacher(models.Model):
     
